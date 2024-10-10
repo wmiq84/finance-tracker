f1c9503910b5217c67b90cb2d19233d119972feb
wmiq84 <michaelwang3489@gmail.com>
2024-10-10 16:37:04 -0700

    Create dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,24 @@ import { createTheme } from '@mui/material/styles';
 import { useMemo } from 'react';
 import { themeSettings } from './theme';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import Dashboard from './scenes/dashboard';
 
 function App() {
-  const theme = useMemo(() => createTheme(themeSettings), []);
-  return (
-    <div className="app">
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-            <Routes>
-              <Route path ="/" element={<p>predictions</p>}></Route>
-            </Routes>
-          </Box>
-        </ThemeProvider>
-      </BrowserRouter>
-    </div>
-  )
+	const theme = useMemo(() => createTheme(themeSettings), []);
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+						<Routes>
+              <Route path="/" element=<Dashboard /> />
+						</Routes>
+					</Box>
+				</ThemeProvider>
+			</BrowserRouter>
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ client/src/scenes/dashboard/index.tsx
@@ -0,0 +1,86 @@
+import { Box, useMediaQuery, useTheme } from '@mui/material';
+import React from 'react';
+import Row1 from './Row1';
+import Row2 from './Row2';
+import Row3 from './Row3';
+
+type Props = {};
+
+const gridTemplateLargeScreens = `
+    "a b c"
+    "a b c"
+    "a b c"
+    "a b f"
+    "d e f"
+    "d e f"
+    "d e h"
+    "g e h"
+    "g e h"
+    "g e h"
+`;
+
+const gridTemplateSmallScreens = `
+    "a"
+    "a"
+    "a"
+    "a"
+    "b"
+    "b"
+    "b"
+    "b"
+    "c"
+    "c"
+    "c"
+    "d"
+    "d"
+    "d"
+    "e"
+    "e"
+    "e"
+    "e"
+    "e"
+    "e"
+    "f"
+    "f"
+    "f"
+    "g"
+    "g"
+    "g"
+    "h"
+    "h"
+    "h"
+    "h"
+`;
+
+const Dashboard = () => {
+	const isAboveMediumScreens = useMediaQuery('(min-width: 1200px)');
+	const { palette } = useTheme();
+	return (
+		<Box
+			width="100%"
+			height="100%"
+			display="grid"
+			gap="1.5rem"
+			color={palette.grey[300]}
+			sx={
+				isAboveMediumScreens
+					? {
+							gridTemplateColumns: 'repeat(3, minmax(370px, 1fr))',
+							gridTemplateRows: 'repeat(10, minmax(60px, 1fr))',
+							gridTemplateAreas: gridTemplateLargeScreens,
+					  }
+					: {
+							gridAutoColumns: '1fr',
+							gridAutoRows: '80px',
+							gridTemplateAreas: gridTemplateSmallScreens,
+					  }
+			}
+		>
+			<Row1 />
+			<Row2 />
+			<Row3 />
+		</Box>
+	);
+};
+
+export default Dashboard;
