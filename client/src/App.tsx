dc5d5f73411ba8e7854114f904487300ebf98372
wmiq84 <michaelwang3489@gmail.com>
2024-10-10 16:17:28 -0700

    Initialize routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { CssBaseline, ThemeProvider, Box } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { useMemo } from 'react';
+import { themeSettings } from './theme';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
-
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <>
-    </>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path ="/" element={<p>predictions</p>}></Route>
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   )
 }
 
