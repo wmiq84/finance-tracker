d5742769047ee09479da53ce4ba7acfdf05f698a
wmiq84 <michaelwang3489@gmail.com>
2024-10-10 17:06:19 -0700

    Create navigation bar

---
+++ client/src/scenes/navbar/index.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import FlexBetween from '@/components/FlexBetween';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import { Box, Typography, useTheme } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+	const { palette } = useTheme();
+	const [selected, setSelected] = useState('dashboard');
+	return (
+		<FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+			{/* Left side of navbar */}
+			<FlexBetween gap="0.75rem">
+				<AttachMoneyIcon sx={{ fontSize: '28px' }}></AttachMoneyIcon>
+				<Typography variant="h4" fontSize="16px">
+					Finance Dashboard
+				</Typography>
+			</FlexBetween>
+			{/* Right side of navbar */}
+			<FlexBetween gap="2rem">
+				<Box sx={{ '&:hover': { color: palette.primary[100] } }}>
+					<Link
+						to="/"
+						onClick={() => setSelected('dashboard')}
+						style={{
+							color: selected === 'dashboard' ? 'inherit' : palette.grey[700],
+							textDecoration: 'inherit',
+						}}
+					>
+						Dashboard
+					</Link>
+                    </Box>
+                    <Box sx={{ '&:hover': { color: palette.primary[100] } }}>
+					<Link
+						to="/predictions"
+						onClick={() => setSelected('predictions')}
+						style={{
+							color: selected === 'predictions' ? 'inherit' : palette.grey[700],
+							textDecoration: 'inherit',
+						}}
+					>
+						Predictions
+					</Link>
+				</Box>
+			</FlexBetween>
+		</FlexBetween>
+	);
+};
+
+export default Navbar;
