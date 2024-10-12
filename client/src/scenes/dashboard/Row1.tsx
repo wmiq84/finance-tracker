564319d66d297beab4929617356c6f504357e8a5
wmiq84 <michaelwang3489@gmail.com>
2024-10-12 15:33:17 -0700

    Add net worth graph

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -69,6 +69,19 @@ const Row1 = (props: Props) => {
 			})
 		);
 	}, [data]);
+
+	const netWorth = useMemo(() => {
+		return (
+			data &&
+			data[0].monthlyData.map(({ month, income, spending }) => {
+				return {
+					name: month.substring(0, 3),
+					netWorth: income - spending,
+				};
+			})
+		);
+	}, [data]);
+
 	return (
 		<>
 			<DashboardBox gridArea="a">
@@ -148,7 +161,59 @@ const Row1 = (props: Props) => {
 					</AreaChart>
 				</ResponsiveContainer>
 			</DashboardBox>
-			<DashboardBox gridArea="b"></DashboardBox>
+			<DashboardBox gridArea="b">
+				<BoxHeader title="Net Worth" sideText="+4%"></BoxHeader>
+				<ResponsiveContainer width="100%" height="100%">
+					<AreaChart
+						width={500}
+						height={400}
+						data={netWorth}
+						margin={{
+							top: 15,
+							right: 25,
+							left: -10,
+							bottom: 60,
+						}}
+					>
+						<defs>
+							<linearGradient id="colorNetWorth" x1="0" y1="0" x2="0" y2="1">
+								<stop
+									offset="5%"
+									stopColor={palette.primary[300]}
+									stopOpacity={0.5}
+								/>
+								<stop
+									offset="95%"
+									stopColor={palette.primary[300]}
+									stopOpacity={0}
+								/>
+							</linearGradient>
+						</defs>
+						<XAxis
+							dataKey="name"
+							tickLine={false}
+							style={{ fontSize: '10px' }}
+							stroke={palette.grey[200]}
+						/>
+						<YAxis
+							tickLine={false}
+							axisLine={{ strokeWidth: '0' }}
+							style={{ fontSize: '10px' }}
+							domain={[1500, 10000]}
+							stroke={palette.grey[200]}
+						/>
+						<Tooltip />
+						<Area
+							type="monotone"
+							dataKey="netWorth"
+							dot={true}
+							stroke={palette.primary.main}
+							fillOpacity={1}
+							fill="url(#colorNetWorth)"
+						/>
+					</AreaChart>
+				</ResponsiveContainer>
+			</DashboardBox>
 			<DashboardBox gridArea="c">
 				<BoxHeader
 					title="Recent Incomes"
